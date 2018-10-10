7f3a0d9c6ab753c5060d3700bac0ad0175353e45
Wenqi <cui.wenq@husky.neu.edu>
2018-10-10 19:36:31 -0400

    Finish part1

--- wranging.py
+++ wranging.py
@@ -6,10 +6,17 @@ Created on Mon Oct  8 15:09:15 2018
 """
 import urllib
 import csv
+import os
+import shutil
+import logging
+import time
+from urllib import request
+logging.basicConfig(filename='logger.log', level=logging.INFO)
+
 table_count = 1;
 def printtable(table,count):
     doc_name="table_"+str(count)+".csv";
-    print(doc_name)
+    logging.info(str(time.asctime( time.localtime(time.time()) ))+"  Create new Table:"+doc_name)
     '''
     headers = [th.text for th in table.select("tr th")]
     with open(doc_name, "w") as f:
@@ -29,23 +36,21 @@ def printtable(table,count):
             data[i] = str(data[i]).replace('b\'', u' ')
             data[i] = str(data[i]).replace('\'', u' ')
         writer.writerow(data)
-       
-		
-		
-		
-		
 
 def geturl(acc):
-    tem = acc.split("-")
-    accs = ""
-    for i in [0,1,2] :
-        accs = accs + tem[i]
-    accn = int(accs)
+    accn = acc[:10]+"-"+acc[10:12]+"-"+acc[12:]
+
     return accn
 
-cik = 51143
-acc = "0000051143-13-000007"
-url = "https://www.sec.gov/Archives/edgar/data/"+str(cik)+"/"+ str(geturl(acc))+"/"+acc+"-index.html"
+
+
+from configparser import ConfigParser
+cfg = ConfigParser()
+cfg.read('company.config')
+cik = cfg.getint('parameter','cik')
+acc = cfg.get('parameter','acc')
+
+url = "https://www.sec.gov/Archives/edgar/data/"+str(cik)+"/"+ str(acc)+"/"+geturl(acc)+"-index.html"
 
 response = urllib.request.urlopen(url)
 content = response.read()
@@ -57,15 +62,52 @@ soup = BeautifulSoup(content,'lxml')
 all_href = soup.find_all('a')
 
 for href in all_href:
-    if ("10q" in href['href']):
+    if (("10q" in href['href'])or("10-q" in href['href'])):
         link_10q = "https://www.sec.gov"+href['href'];
 content_10q = urllib.request.urlopen(link_10q).read()
 
 soup = BeautifulSoup(content_10q,'lxml')
 table_div = soup.find_all("table",style="border:none;border-collapse:collapse;width:100%;",)
+if (not os.path.exists("tables")):
+    os.mkdir("tables")
+    logging.info(str(time.asctime( time.localtime(time.time()) ))+"Make Direction: tables")
+os.chdir("tables")
 for div in table_div:
     printtable(div,table_count)
     table_count += 1
+os.chdir("..")
+shutil.make_archive("data","zip","tables")
+logging.info(str(time.asctime( time.localtime(time.time()) ))+"Make archive")
+
+'''
+import boto3
+s3 = boto3.resource('s3')
+s3.meta.client.upload_file('data.zip','wenqi.7390','data.zip')
+'''
+import boto
+import boto.s3
+from boto.s3.key import Key
+
+
+#Need your own aws key
+cfg.read('aws.config')
+AWS_ACCESS_KEY_ID = cfg.get('parameter','AWSAccessKeyId')
+AWS_SECRET_ACCESS_KEY = cfg.get('parameter','AWSSecretKey')
+
+
+bucket_name = "7390wenqi"
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY)
+
+bucket = conn.get_bucket(bucket_name)
+k = Key(bucket)
+k.key = str(cik)+"data.zip"
+k.set_contents_from_filename("data.zip")
+k.key =  str(cik)+"logger.log"
+k.set_contents_from_filename("logger.log")
+
+
+
 
 
         
